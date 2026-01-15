9368c5dfd568ff4279a80fbbfbb1898b58bc92b0
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-01-15 09:46:51 +0300

    changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.schemas import user, podcast, comment, category, favorite
+from .schemas import user, podcast, comment, category, favorite
 
 from app.api import (
     auth_router,
